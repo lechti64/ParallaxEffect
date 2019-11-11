0848a36e9a87fd3a893814b02467cdb121fd6afd
lechti64 <lechti64@users.noreply.github.com>
2019-11-11 09:29:00 +0100

    Update effectparallax.js

--- effectparallax.js
+++ effectparallax.js
@@ -1,26 +1,18 @@
 function effectParallax() {
-
     var effect = 1.8;
     var elm = document.getElementsByClassName("parallax");
     var i = 0;
-
     while (elm[i]) {
         elmoffsetTop = elm[i].offsetTop;
         elmoffsetWidth = elm[i].offsetWidth;
         scrollPos = window.pageYOffset;
         scrollElm = scrollPos-elmoffsetWidth;
-
         if (scrollPos >= elmoffsetTop) {
-
           if (scrollElm <= elmoffsetTop) {
-
               elm[i].style.transform =  "translateY(" + ((scrollPos-elmoffsetTop)/effect) +"px" + ")";
-
           }
-
         }
         i++;
     }
-
 }
 window.addEventListener("scroll", effectParallax);
